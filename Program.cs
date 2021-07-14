368739618bf0999bc06766b2f5860a8e0bee39bf
Lucas Wolf <lucasgiovanew@gmail.com>
2021-07-14 15:10:45 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Integracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new org.oorsprong.webservices.CountryInfoService { Url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL" };
+
+            foreach (var country in service.ListOfCountryNamesByName())
+            {
+                Console.WriteLine(country.sName.ToString());
+            }
+
+            while(true)
+            {
+                Console.WriteLine("\nInforme um dos países acima para receber informações sobre ele.");
+
+                string typedCountry = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(typedCountry))
+                {
+                    typedCountry = char.ToUpper(typedCountry[0]) + typedCountry.Substring(1).ToLower();
+                    string countryCode = service.CountryISOCode(typedCountry);
+
+                    if (countryCode == "No country found by that name")
+                    {
+                        Console.WriteLine("Nenhum país encontrado!");
+                    }
+                    else
+                    {
+                        var currency = service.CountryCurrency(countryCode);
+                        Console.WriteLine("Moeda: " + currency.sName + "(" + currency.sISOCode + ")");
+                        Console.WriteLine("Código Telefônico: +" + service.CountryIntPhoneCode(countryCode));
+                    }
+                }
+            }
+        }
+    }
+}
